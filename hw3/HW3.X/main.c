ebf6db6190c32564e34621bbcdb95d0b1a3fa90c
Marno (Mathinus) Nel <marthinusnel2023@u.northwestern.edu>
2023-04-04 21:47:23 -0500

    Now works with button press now

--- hw3/HW3.X/main.c
+++ hw3/HW3.X/main.c
@@ -13,38 +13,24 @@ int main(void) {
   
   NU32DIP_Startup(); // cache on, interrupts on, LED/button init, UART init
   while (1) {
-// Linear lines
-//    for (int i = 0; i < 100; i++) {
-//        sprintf(message,"%f\r\n",i*3.3/100.0);
-//        NU32DIP_WriteUART1(message);
-//        _CP0_SET_COUNT(0);
-//        while(_CP0_GET_COUNT()<24000000/1000) {} // Wait 1ms between points
-//    }
-    WaveGenerator(wave, data_points, amplitude);
-
-
+    // Linear lines
+    //    for (int i = 0; i < 100; i++) {
+    //        sprintf(message,"%f\r\n",i*3.3/100.0);
+    //        NU32DIP_WriteUART1(message);
+    //        _CP0_SET_COUNT(0);
+    //        while(_CP0_GET_COUNT()<24000000/1000) {} // Wait 1ms between points
+    //    }
     
 	if (!NU32DIP_USER){ // Thus when the button is pressed send the sine wave
         // Every time you push the USER button, send a single cycle of a 
         // sine wave in 100 data points, with a 0.01 second delay between 
         // each data point.
-//        WaveGenerator(freq, amplitude);
+        WaveGenerator(wave, data_points, amplitude);
 	}
   }
 }
 
 // Generate a sine wave
-//void WaveGenerator(int freq, float amplitude){
-//    for (int i = 0; i < 100; i++) {
-//        sprintf(message,"%f\r\n",i*3.3/100.0);
-//        NU32DIP_WriteUART1(message);
-//        _CP0_SET_COUNT(0);
-//        while(_CP0_GET_COUNT()<24000000/1000) {} // Wait 1ms between points
-//    }
-//}
-
-
-
 void WaveGenerator(float* wave, int size, float amplitude) {
     float frequency = 1.0 / size;
     for (int i = 0; i < size; i++) {
@@ -53,6 +39,6 @@ void WaveGenerator(float* wave, int size, float amplitude) {
         sprintf(message, "%f\r\n", amplitude * sin(angle));
         NU32DIP_WriteUART1(message);
         _CP0_SET_COUNT(0);
-        while(_CP0_GET_COUNT()<24000000/1000) {} // Wait 1ms between points
+        while(_CP0_GET_COUNT()<24000000/100) {} // Wait 1ms between points
     }
 }
