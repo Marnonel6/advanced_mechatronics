5736bfd4e8ff65f0a1aa63225bf82574f22d0113
Marno (Mathinus) Nel <marthinusnel2023@u.northwestern.edu>
2023-06-07 20:55:39 -0500

    Cleaned code

--- hw16/HW16.X/main.c
+++ hw16/HW16.X/main.c
@@ -18,82 +18,15 @@ int main(void) {
                 char line_center[100];
                 sprintf(line_center,"%d\n\n\n\r",com);
                 NU32DIP_WriteUART1(line_center);
-                
-//        // P Controller
-          // Somewhat follows the line, but stalls and is very slow.
-//        int set_point = 30;
-//        int Kp = 1.0;
-//        int error = 0;
-//        int max_pwm = 70;
-//        int min_pwm = 20;
-//        int base_pwm = 30;
-//        
-//        error = com - set_point;
-//        
-//        MOTOR_1_DIRECTION = 1; // Forward -> Left wheel
-//        MOTOR_2_DIRECTION = 0; // Forward -> Right wheel
-//        
-//        int left_pwm = error*Kp + base_pwm;
-//        int right_pwm = -error*Kp + base_pwm;
-//        
-//        if (left_pwm >= max_pwm){
-//            MOTOR_1_SPEED = (int)(PR3_PERIOD*(float)max_pwm/100.0);
-//        }
-//        else if (left_pwm <= min_pwm){
-//            MOTOR_1_DIRECTION = 0;
-//            MOTOR_1_SPEED = (int)(PR3_PERIOD*(float)base_pwm/100.0);
-//        }
-//        else {
-//            MOTOR_1_SPEED = (int)(PR3_PERIOD*(float)left_pwm/100.0);
-//        }
-//        
-//        if (right_pwm >= max_pwm){
-//            MOTOR_2_SPEED = (int)(PR3_PERIOD*(float)max_pwm/100.0);
-//        }
-//        else if (right_pwm <= min_pwm){
-//            MOTOR_2_DIRECTION = 1;
-//            MOTOR_2_SPEED = (int)(PR3_PERIOD*(float)base_pwm/100.0);
-//        }
-//        else {
-//            MOTOR_2_SPEED = (int)(PR3_PERIOD*(float)right_pwm/100.0);
-//        }
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-        // P Controller -> WORKS GREAT JUST HOLD A LIGHT
-                
-          // Works !
-//        int set_point = 30;
-//        int Kp = 1.5;
-//        int error = 0;
-//        int max_pwm = 50;
-//        int min_pwm = 0;
-//        int base_pwm = 25;
-                
-        // Best one
+                          
+        // P Controller -> WORKS GREAT JUST HOLD A LIGHT    
         int set_point = 30;
         int Kp = 1.5; //1.2;
         int error = 0;
         int max_pwm = 70;
         int min_pwm = 0;
         int base_pwm = 35;
-                
-                
-//        int set_point = 30;
-//        int Kp = 2.0;
-//        int error = 0;
-//        int max_pwm = 100;
-//        int min_pwm = 0;
-//        int base_pwm = 30;
-//        
+
         error = com - set_point;
         
         MOTOR_RIGHT_DIRECTION = 1; // Forward -> Right wheel
@@ -148,17 +81,7 @@ int main(void) {
 //            while(_CP0_GET_COUNT()<24000000/20) {} // Wait 5ms //20
 //        }
 //                
-       
-//        _CP0_SET_COUNT(0);
-//        while(_CP0_GET_COUNT()<24000000/10) {} // Wait 10ms
-//        
-//        MOTOR_LEFT_SPEED = (int)(PR3_PERIOD*(float)0.0/100.0);
-//        MOTOR_RIGHT_SPEED = (int)(PR3_PERIOD*(float)0.0/100.0);
-//        
-//        _CP0_SET_COUNT(0);
-//        while(_CP0_GET_COUNT()<24000000/20) {} // Wait 5ms
-                
-                
+
                 
 //        // P Controller -> 5 sections ->>>> Pretty okay
 //        int set_point = 30;
@@ -266,79 +189,7 @@ int main(void) {
 //        }
 //        
 //        MOTOR_RIGHT_SPEED = (int)(PR3_PERIOD*(float)right_pwm/100.0);
-//        MOTOR_LEFT_SPEED = (int)(PR3_PERIOD*(float)left_pwm/100.0);
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//        
-//        MOTOR_1_DIRECTION = 1; // Forward -> Left wheel
-//        MOTOR_2_DIRECTION = 0; // Forward -> Right wheel
-//        
-//        int left_pwm = error*Kp + base_pwm;
-//        int right_pwm = -error*Kp + base_pwm;
-//        
-//        if (left_pwm >= max_pwm){
-//            MOTOR_1_SPEED = (int)(PR3_PERIOD*(float)max_pwm/100.0);
-//        }
-//        else if (left_pwm <= min_pwm){
-//            MOTOR_1_DIRECTION = 0;
-//            MOTOR_1_SPEED = (int)(PR3_PERIOD*(float)base_pwm/100.0);
-//        }
-//        else {
-//            MOTOR_1_SPEED = (int)(PR3_PERIOD*(float)left_pwm/100.0);
-//        }
-//        
-//        if (right_pwm >= max_pwm){
-//            MOTOR_2_SPEED = (int)(PR3_PERIOD*(float)max_pwm/100.0);
-//        }
-//        else if (right_pwm <= min_pwm){
-//            MOTOR_2_DIRECTION = 1;
-//            MOTOR_2_SPEED = (int)(PR3_PERIOD*(float)base_pwm/100.0);
-//        }
-//        else {
-//            MOTOR_2_SPEED = (int)(PR3_PERIOD*(float)right_pwm/100.0);
-//        }
-        
-        
-        
-        
-        
-//        if (com == 1000) { // No line
-//            MOTOR_1_DIRECTION = 1; // Forward -> Left wheel
-//            MOTOR_1_SPEED = (int)(PR3_PERIOD*(float)50.0/100.0);
-//            MOTOR_2_DIRECTION = 1; // Backward -> Right wheel
-//            MOTOR_2_SPEED = (int)(PR3_PERIOD*(float)50.0/100.0);
-//        }       
-//        else if (com <= 32 && com >= 28) { 
-//            MOTOR_1_DIRECTION = 1; // Forward -> Left wheel
-//            MOTOR_1_SPEED = (int)(PR3_PERIOD*(float)80.0/100.0);
-//            MOTOR_2_DIRECTION = 0; // Forward -> Right wheel
-//            MOTOR_2_SPEED = (int)(PR3_PERIOD*(float)80.0/100.0);
-//        }
-//        else if (com < 28) {
-//            MOTOR_1_DIRECTION = 1; // Forward -> Left wheel
-//            MOTOR_1_SPEED = (int)(PR3_PERIOD*(float)40.0/100.0);
-//            MOTOR_2_DIRECTION = 0; // Forward -> Right wheel
-//            MOTOR_2_SPEED = (int)(PR3_PERIOD*(float)70.0/100.0);
-//        }
-//        else if (com > 32) {
-//            MOTOR_1_DIRECTION = 1; // Forward -> Left wheel
-//            MOTOR_1_SPEED = (int)(PR3_PERIOD*(float)70.0/100.0);
-//            MOTOR_2_DIRECTION = 0; // Forward -> Right wheel
-//            MOTOR_2_SPEED = (int)(PR3_PERIOD*(float)40.0/100.0);
-//        }
-        
+//        MOTOR_LEFT_SPEED = (int)(PR3_PERIOD*(float)left_pwm/100.0);             
     }
     }
 }
